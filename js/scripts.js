2e5fab6a76ecf5d9305b97a6af583664df4633a9
Cathy Bradley (ctb116) <catherinetybradley@gmail.com>
2020-10-29 01:28:51 -0700

    remove jquery script with js equivalents

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,31 @@
+//window object
 console.log(window);
+// Represents the browser's window, each tab is a window
+// is global
+// check it out in the browser Console
 
-alert("This alert fires before the page loads.")
+// comes with a lot of properties:
+console.log("window height: " + window.innerHeight);
+console.log("window width: " + window.innerWidth);
 
-$(document).ready(function () {
+// comes with a lot of functions. Like alert()!
+window.alert("This alert fired before the page was finished loading.");
 
-	alert("This alert fired after the page finished loading");
+// NOTE - no need to do window. before everything as you've seen before using alerts
 
-	console.log(document);
+// document object
+console.log(window.document); //same as console.log(document);
+// represents the web page. Can access all page content
+// check it out in the browser console
 
-});
+function pageReady() {
+	alert("This alert fired after the page finished loading.");
+};
 
+
+// PRACTICE in the browser Console
+function callFromConsole(message) {
+	alert(message);
+}
+// Declare a variable
+// Pass that variable as an argument to callFromConsole in the browser console
